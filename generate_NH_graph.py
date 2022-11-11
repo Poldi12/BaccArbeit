1dce38515eb7c4b82915f6f44d1bc1d9c271561f
Poid12 <atzquell.l@gmail.com>
2022-11-11 14:15:30 +0100

    print fixed, some improvements to graph generation

--- generate_NH_graph.py
+++ generate_NH_graph.py
@@ -6,13 +6,13 @@ from dataclasses import dataclass
 
 @dataclass
 class LocalView:
-    my_color: int
+    my_color: int = None
     neighbor_colors = []
-    can_be_adjacent: bool
+    can_be_adjacent: bool = None
 
 @dataclass
 class Ball:
-    mylocalview: LocalView
+    mylocalview: LocalView = None
 
 @dataclass
 class NHGraph:
@@ -26,27 +26,24 @@ def generateNeighborhoodGraph(rounds: int, max_color: int, degree: int, NH_graph
     #generate balls
     for color in range(max_color):
 
-        ball_ = Ball
-        ball_.mylocalview = LocalView
+        ball_ = Ball()
+        ball_.mylocalview = LocalView()
 
-        for combination in range(max_color):
-            ball_.mylocalview.my_color = color
+        ball_.mylocalview.my_color = color
 
-            #set neighbor colors
-            neighboroffset: int = 0
-            for neighbors in range(degree):
-                if(((neighbors + neighboroffset) != color) and ((neighbors + neighboroffset) <= max_color)):
-                    ball_.mylocalview.neighbor_colors.append(neighbors + neighboroffset)
-                neighboroffset += 1
+        #set neighbor colors
+        for neighbors in range(degree):
+            if(((neighbors) != color) and ((neighbors) <= max_color)):
+                ball_.mylocalview.neighbor_colors.append(neighbors)
 
         NHGraph_.SetOfBall.append(ball_)
 
-        print_NHGraph(NHGraph_)
+    print_NHGraph(NHGraph_)
     return 0
 
 def print_NHGraph(NHGraph: NHGraph):
     for i in range(len(NHGraph.SetOfBall)):
-        print(str(NHGraph.SetOfBall[i].mylocalview.my_color) +"/n")
+        print(str(NHGraph.SetOfBall[i].mylocalview.my_color))
         for j in range(len(NHGraph.SetOfBall[i].mylocalview.neighbor_colors)):
-            print(str(NHGraph.SetOfBall[i].mylocalview.neighbor_colors[j]) + " ")
-        print("/n")
+            print(str(NHGraph.SetOfBall[i].mylocalview.neighbor_colors[j]) + " ", end = '')
+        print("")
